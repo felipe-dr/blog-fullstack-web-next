179427fd121b0499243da3ed972338f7f07c7981
Felipe DR <felipe.corp7@gmail.com>
2024-05-30 15:15:53 -0300

    feat: class-01 starting the foundations of the project

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Box from "@src/components/box";
+import theme from "@src/theme/theme";
+
+export default function HomeScreen() {
+  return (
+    <Box
+      tag="main"
+      styleSheet={{
+        fontFamily: theme.typography.fontFamily,
+        backgroundColor: {
+          xs: "red",
+          sm: "yellow",
+          md: "blue",
+        },
+      }}
+    >
+      Oi
+    </Box>
+  );
+}
